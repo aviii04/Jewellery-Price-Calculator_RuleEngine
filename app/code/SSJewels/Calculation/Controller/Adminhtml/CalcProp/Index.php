b95a0f70a96dc432e9afabff7263f7e144c0987c
Avinash Thakur <imthakuravinash1994@gmail.com>
2021-02-11 02:11:42 +0530

    Adding Admin Menu

---
+++ app/code/SSJewels/Calculation/Controller/Adminhtml/CalcProp/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace SSJewels\Calculation\Controller\Adminhtml\CalcProp;
+
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+
+    protected $pageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $pageFactory
+     */
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend((__('Posts')));
+
+        return $resultPage;
+    }
+}
---
+++ app/code/SSJewels/Calculation/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'SSJewels_Calculation',
+    __DIR__
+);
